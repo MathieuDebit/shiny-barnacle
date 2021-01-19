c83841eb75cd089817598b8c1b8a3e108c2ba31b
Mathieu Débit <mathieu+github@debit.email>
2021-01-19 10:58:35 +0100

    feat(app): Add GraphQL support
    - Add a simple query that display information about a country

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import type { AppProps } from 'next/app';
 import 'src/styles/globals.css';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://countries.trevorblades.com/',
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
 export default MyApp;
--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,15 +1,39 @@
-import Head from 'next/head';
+import { useQuery, gql } from '@apollo/client';
 import styles from './styles.module.css';
 
+const GET_COUNTRY = gql`
+  query GetCountry($code: ID!) {
+    country(code: $code) {
+      name
+      emoji
+      languages {
+        code
+        name
+      }
+    }
+  }
+`;
+
 export default function HomeTemplate() {
+  const { loading, error, data } = useQuery(GET_COUNTRY, {
+    variables: { code: 'FR' },
+  });
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error</div>;
+
   return (
     <div className={styles.container}>
-      <Head>
-        <title>shiny-barnacle</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      {`${data.country.name} ${data.country.emoji}`}
 
-      <div>Hello</div>
+      <div>Languages</div>
+      <ul>
+        {data.country.languages.map(({ code, name }: { code: string; name: string }) => (
+          <li key={code}>
+            {`${name} (${code})`}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
